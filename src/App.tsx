3f8aaf3b8b4955ed2959f99bbb63b5e2d8983708
heiwa4126 <heiwa4126@gmail.com>
2024-10-30 13:09:55 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import "./App.css";
 import App1 from "./App1";
 import App2 from "./App2";
 import App3 from "./App3";
+import App3a from "./App3a";
 import App4 from "./App4";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<App4 />} />
-				<Route path="/index.html" element={<App4 />} />
+				<Route path="/" element={<App3a />} />
+				<Route path="/index.html" element={<App3a />} />
 				{/* ↑デフォルトアプリ */}
 				<Route path="/1" element={<App1 />} />
 				<Route path="/2" element={<App2 />} />
 				<Route path="/3" element={<App3 />} />
+				<Route path="/3a" element={<App3a />} />
 				<Route path="/4" element={<App4 />} />
 			</Routes>
 		</>
---
+++ src/MySelect.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export interface SelectData<T> {
+	data: T;
+	label: string;
+}
+
+// <select>のocChangeイベントとvalue以外をpropsで渡せるようになっている
+interface MySelectProps<T> extends Omit<JSX.IntrinsicElements["select"], "onChange" | "value"> {
+	dataArray: Array<SelectData<T>>;
+	onChange: (data: T) => void;
+	valueIdx: number;
+}
+
+export function MySelect<T>({ dataArray, valueIdx, onChange, ...selectProps }: MySelectProps<T>) {
+	const [selectedIndex, setSelectedIndex] = useState(valueIdx);
+
+	const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const newIndex = Number.parseInt(event.target.value, 10);
+		setSelectedIndex(newIndex);
+		onChange(dataArray[newIndex].data);
+	};
+
+	return (
+		<select onChange={handleSelectChange} value={selectedIndex} {...selectProps}>
+			{dataArray.map(({ label }, index) => (
+				<option key={`${index}${label}`} value={index}>
+					{label}
+				</option>
+			))}
+		</select>
+	);
+}
