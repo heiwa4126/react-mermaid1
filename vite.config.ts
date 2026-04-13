0fbd1811eb09b6ce3a9bce4d612f6148d148d85d
heiwa4126 <heiwa4126@gmail.com>
2026-04-13 10:40:29 +0900

    [WIP] update modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,31 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import cdn from "vite-plugin-cdn-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		cdn({
-			modules: [
-				"react",
-				"react-dom",
-				{
-					name: "mermaid",
-					var: "mermaid",
-					path: "dist/mermaid.min.js",
-				},
-			],
-		}),
+		// cdn({
+		// 	modules: [
+		// 		"react",
+		// 		"react-dom",
+		// 		{
+		// 			name: "mermaid",
+		// 			var: "mermaid",
+		// 			path: "dist/mermaid.min.js",
+		// 		},
+		// 	],
+		// }),
 	],
 	base: process.env.GITHUB_REPO_NAME ?? "./",
 	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					r: ["react-router"],
-				},
-			},
-		},
+		// rollupOptions: {
+		// 	output: {
+		// 		manualChunks: {
+		// 			r: ["react-router"],
+		// 		},
+		// 	},
+		// },
 	},
 	esbuild: {
 		drop: ["console", "debugger"], // https://esbuild.github.io/api/#drop
